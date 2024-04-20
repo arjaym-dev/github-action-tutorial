7ee03561302a18073306fdfba8003605cf4ae30e
arjaym-dev <arjaym.dev@gmail.com>
2024-04-20 11:12:05 +0800

    Setup simple app

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <React.Fragment>
+      <header>This is header</header>
+      <main>
+        <h1>Github action demo</h1>
+      </main>
+      <footer>This is footer</footer>
+    </React.Fragment>
+  );
+}
